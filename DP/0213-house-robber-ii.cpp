c3c4a2f9b139bdf5419ae2a872ae33c0742d971e
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-01-23 19:31:54 +0530

    house-robber-ii

---
+++ DP/0213-house-robber-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  int rob(vector<int>& nums) {
+    int n = nums.size();
+    
+    if (n == 1) 
+      return nums[0];
+    
+    return max(f(nums, 0, n-2), f(nums, 1, n-1));
+  }
+
+  int f(vector<int>& nums, int l, int r) {
+    int rob1 = 0, rob2 = 0;
+
+    for (int i = l; i <= r; ++i) {
+      int tmp = max(rob1 + nums[i], rob2);
+      rob1 = rob2;
+      rob2 = tmp;
+    }
+    
+    return rob2;
+  }
+};
