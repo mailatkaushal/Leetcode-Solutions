bf432eae5328f33ef96b58a29ca83e5957909cf8
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-01-24 21:02:49 +0530

    determine-if-two-strings-are-close

---
+++ Strings/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,27 @@
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+public:
+  bool closeStrings(string w1, string w2) {
+    if (w1.size() != w2.size())
+      return false;
+
+    vector<int> v(26, 0), w(26, 0);
+
+    for (int i = 0; i < w1.size(); ++i) {
+      ++v[w1[i] - 'a'];
+      ++w[w2[i] - 'a'];
+    }
+
+    for (int i = 0; i < 26; ++i) {
+      if ((v[i] > 0 && w[i] == 0) || (v[i] == 0 && w[i] > 0))
+        return false;
+    }
+
+    sort(v.begin(), v.end());
+    sort(w.begin(), w.end());
+
+    return v == w;
+  }
+};
