29c1f3505c2c36d67490b65f7178c43d9437e156
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-01-24 19:45:32 +0530

    powx-n

---
+++ Maths/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+// Time: O(logn)
+// Space: O(1)
+
+class Solution {
+public:
+  double myPow(double x, int n) {
+    double res = 1;
+    long long exp = n;
+
+    if (exp < 0) {
+      x = 1 / x;
+      exp = -exp;
+    }
+
+    while (exp) {
+      if (exp % 2 == 1)
+        res *= x;
+      x *= x;
+      exp >>= 1;
+    }
+
+    return res;
+  }
+};
