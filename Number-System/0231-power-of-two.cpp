842a7f15b4dde7a6913edde1c5a2d2df2636c102
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-02-19 23:29:59 +0530

    power-of-two

---
+++ Number-System/0231-power-of-two.cpp
@@ -0,0 +1,37 @@
+// Time  : O(logn)
+// Space  : O(1)
+
+class Solution:
+  def isPowerOfTwo(self, n: int) -> bool:
+    x = 1
+    while x < n:
+      x *= 2
+    return x == n
+
+// Time  : O(logn)
+// Space  : O(1)
+
+class Solution {
+public:
+  bool isPowerOfTwo(int n) {
+    int sum_bits = 0;
+
+    while (n > 0) {
+      int bit = n & 1;
+      sum_bits += bit;
+      n = n >> 1;
+    }
+
+    return sum_bits == 1;
+  }
+};
+
+// Time  : O(1)
+// Space  : O(1)
+
+class Solution:
+  def isPowerOfTwo(self, n: int) -> bool:
+    if n <= 0:
+      return False
+
+    return n & (n - 1) == 0
