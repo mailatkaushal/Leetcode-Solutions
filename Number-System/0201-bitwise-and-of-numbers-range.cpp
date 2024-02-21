6cc2588dc5effb268a69f63b4361706cf7ff3722
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-02-21 15:21:57 +0530

    update

--- Number-System/0201-bitwise-and-of-numbers-range.cpp
+++ Number-System/0201-bitwise-and-of-numbers-range.cpp
@@ -35,3 +35,17 @@ class Solution:
 
     # Time  : O(32) ~ O(1)
     # Space : O(1)
+
+# Time  : O(32) ~ O(1)
+# Space : O(1)
+
+class Solution:
+  def rangeBitwiseAnd(self, left: int, right: int) -> int:
+    shift_count = 0
+
+    while (left != right):
+      left >>= 1
+      right >>= 1
+      shift_count += 1
+
+    return left << shift_count
