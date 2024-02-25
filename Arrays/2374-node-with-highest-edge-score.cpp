923dc723df3ea3f09a07e0468df4a41c4f201dc5
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-02-25 10:12:05 +0530

    node-with-highest-edge-score

---
+++ Arrays/2374-node-with-highest-edge-score.cpp
@@ -0,0 +1,17 @@
+class Solution:
+  def edgeScore(self, edges: List[int]) -> int:
+    n = len(edges)
+    score = [0] * n  # score[i] -> sum of all nodes that have an edge pointing to i
+
+    for i in range(n):
+      score[edges[i]] += i
+
+    node = -1
+    max_score = -1
+
+    for i in range(n):
+      if score[i] > max_score:
+        max_score = score[i]
+        node = i
+
+    return node
