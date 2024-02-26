2d457d35295c48cbde1960327077c8f2a77071c3
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-02-26 10:56:27 +0530

    added bfs solution

--- Trees/0100-same-tree.cpp
+++ Trees/0100-same-tree.cpp
@@ -1,9 +1,37 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+// Time  : O(n)
+// Space : O(n)
+
+class Solution {
+public:
+  bool isSameTree(TreeNode* a, TreeNode* b) {
+    queue<TreeNode*> q1, q2;
+    q1.push(a);
+    q2.push(b);
+
+    while (!q1.empty() && !q2.empty()) {
+      TreeNode* node1 = q1.front(); 
+      q1.pop();
+      TreeNode* node2 = q2.front(); 
+      q2.pop();
+      
+      if (node1 == nullptr && node2 == nullptr) 
+        continue;
+      else if (node1 == nullptr || node2 == nullptr || node1->val != node2->val)
+        return false;
+
+      q1.push(node1->left);
+      q2.push(node2->left);
+      q1.push(node1->right);
+      q2.push(node2->right);
+    }
+
+    return true;
+  }
+};
+
+// Time  : O(n)
+// Space : O(1)
+
 class Solution:
   def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
     # both null -> identical
