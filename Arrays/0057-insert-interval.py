0f23e0ccf9fd989a81200cab546b8b8adaa456be
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-02 12:47:01 +0530

    insert-interval

--- Arrays/0057-insert-interval.py
+++ Arrays/0057-insert-interval.py
@@ -3,27 +3,46 @@
 
 class Solution:
   def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    n = len(intervals)
+    i = 0
     res = []
 
-    for interval in intervals:
-      # Non-overlapping 
-      # interval comes before newInterval. Add interval to res
-      if interval[1] < newInterval[0]:
-        res.append(interval)
-        
-      # Non-overlapping
-      # newInterval comes before interval. Add newInterval to res. Now, newInterval becomes interval
-      elif newInterval[1] < interval[0]:
-        res.append(newInterval)
-        newInterval = interval
+    # Case 1: Non-overlapping intervals before newInterval
+    while i < n and intervals[i][1] < newInterval[0]:
+      res.append(intervals[i])
+      i += 1
 
-      # Overlapping
-      else:
-        # update the newInterval
-        newInterval[0] = min(interval[0], newInterval[0])
-        newInterval[1] = max(interval[1], newInterval[1])
-
-    # Add the last non-overlapping interval(newInterval)
+    # Case 2: Overlapping, merging interval and newInterval
+    while i < n and newInterval[1] >= intervals[i][0]:
+      newInterval[0] = min(intervals[i][0], newInterval[0])
+      newInterval[1] = max(intervals[i][1], newInterval[1])
+      i += 1
+    
+    # Add the non-overlapping newInterval
     res.append(newInterval)
 
+    # Case 3: Non-overlapping intervals after newInterval added
+    while i < n:
+      res.append(intervals[i])
+      i += 1
+
     return res
+
+    '''
+    Case 1:
+      ________    ________
+         i          newI
+
+    Case 2: 
+
+         i                        i               i
+      ________                 ________        ________  
+          ________     ________            ________
+            newI         newI                newI
+
+    Case 3:
+
+        newI         i
+      ________    ________
+    
+    '''
