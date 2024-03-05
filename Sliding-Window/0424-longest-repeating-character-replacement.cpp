0a31ec2d469509d6152f3605c94c77e4ca39627b
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-05 17:57:33 +0530

    longest-repeating-character-replacement

---
+++ Sliding-Window/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,21 @@
+// Time  : O(n)
+// Space : O(26) ~ O(1)
+
+class Solution {
+public:
+  int characterReplacement(string s, int k) {
+    vector<int> cnt(26, 0);
+    int maxCnt = 0, maxLen = 0;
+
+    for (int l = 0, r = 0; r < s.size(); ++r) {
+      maxCnt = max(maxCnt, ++cnt[s[r] - 'A']);
+      if ((r - l + 1) - maxCnt > k) {  // size of the window - largest count of a single, unique char in the window = 
+        --cnt[s[l] - 'A'];
+        ++l;
+      }
+      maxLen = max(maxLen, r - l + 1);
+    }
+
+    return maxLen;
+  }
+};
