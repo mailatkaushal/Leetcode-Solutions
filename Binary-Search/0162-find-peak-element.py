bed492351ecda52a3f59fb87d264cd3506e3df80
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-09 00:52:01 +0530

    find-peak-element

---
+++ Binary-Search/0162-find-peak-element.py
@@ -0,0 +1,24 @@
+# Time  : O(logn)
+# Space : O(1)
+
+class Solution:
+  def findPeakElement(self, A: List[int]) -> int:
+    n = len(A)
+
+    if n == 1:
+      return 0
+    if A[0] > A[1]:
+      return 0
+    if A[n-2] < A[n-1]:
+      return n-1
+
+    lo, hi = 1, n - 2
+
+    while lo <= hi:
+      mid = lo + (hi - lo) // 2
+      if A[mid - 1] < A[mid] > A[mid + 1]:  
+        return mid
+      elif A[mid] < A[mid + 1]:
+        lo = mid + 1
+      else:
+        hi = mid - 1
