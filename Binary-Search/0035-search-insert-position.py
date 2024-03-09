911f66819e08ac22512021117dd4f58d3958b5ac
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-09 18:09:45 +0530

    update

--- Binary-Search/0035-search-insert-position.py
+++ Binary-Search/0035-search-insert-position.py
@@ -1,3 +1,6 @@
+# Time  : O(logn)
+# Space : O(1)
+
 class Solution:
   def searchInsert(self, A: List[int], target: int) -> int:
     lo, hi = 0, len(A) - 1
@@ -11,4 +14,19 @@ class Solution:
       else:
         return mid
 
+    return lo
+
+# Time  : O(logn)
+# Space : O(1)
+
+class Solution:
+  def searchInsert(self, A: List[int], target: int) -> int:
+    # lower bound
+    lo, hi = 0, len(A)
+    while lo < hi:
+      mid = lo + (hi - lo) // 2
+      if A[mid] < target:
+        lo = mid + 1
+      else:
+        hi = mid
     return lo
