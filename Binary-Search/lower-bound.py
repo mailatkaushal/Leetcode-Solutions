abd0b0be7f5a3b45a55fd9d38231b6f4651af5bf
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-14 23:36:47 +0530

    added definition

--- Binary-Search/lower-bound.py
+++ Binary-Search/lower-bound.py
@@ -1,3 +1,5 @@
+# Lower bound - finds the first index where the value at that index is greater than or equal to target
+
 def lowerBound(A: [int], n: int, x: int) -> int:
   lo, hi = 0, n
   while lo < hi:
--- Binary-Search/upper-bound.py
+++ Binary-Search/upper-bound.py
@@ -1,3 +1,5 @@
+# Upper Bound - finds the first index where the value at that index is greater than to target
+
 def upperBound(A: [int], x: int, n: int) -> int:
   lo, hi = 0, n
   while lo < hi:
