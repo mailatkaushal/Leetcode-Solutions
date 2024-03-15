f6783dd05e439d4cce2328334bc54b70e7e1638a
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-15 13:22:28 +0530

    update

--- Binary-Search/lower-bound.py
+++ Binary-Search/lower-bound.py
@@ -13,6 +13,16 @@ def lowerBound(A: [int], n: int, x: int) -> int:
     return lb
 
 def lowerBound(A: [int], n: int, x: int) -> int:
+  lo, hi = 0, n - 1
+  while lo <= hi:
+    mid = lo + (hi - lo) // 2
+    if A[mid] < x:
+      lo = mid + 1
+    else:
+      hi = mid - 1
+  return lo
+
+def lowerBound(A: [int], n: int, x: int) -> int:
   lo, hi = 0, n
   while lo < hi:
     mid = lo + (hi - lo) // 2
