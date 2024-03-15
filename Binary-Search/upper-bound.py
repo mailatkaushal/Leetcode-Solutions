f1a6d7b6b7db4813ff939d0c05f2a0ec304f7a4e
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-15 13:24:24 +0530

    update

--- Binary-Search/upper-bound.py
+++ Binary-Search/upper-bound.py
@@ -13,6 +13,16 @@ def upperBound(A: [int], x: int, n: int) -> int:
   return ub
 
 def upperBound(A: [int], x: int, n: int) -> int:
+  lo, hi = 0, n - 1
+  while lo <= hi:
+    mid = lo + (hi - lo) // 2
+    if A[mid] <= x:
+      lo = mid + 1
+    else:
+      hi = mid - 1
+  return lo 
+
+def upperBound(A: [int], x: int, n: int) -> int:
   lo, hi = 0, n
   while lo < hi:
     mid = lo + (hi - lo) // 2
