329eedeeb9b8a7baf8865af87f705527631c5318
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-21 01:10:32 +0530

    update

--- Linked-List/0876-middle-of-the-linked-list.cpp
+++ Linked-List/0876-middle-of-the-linked-list.cpp
@@ -1,3 +1,26 @@
+// Time  : O(n + n/2)
+// Space : O(1)
+
+class Solution {
+public:
+  ListNode* middleNode(ListNode* head) {
+    int cnt = 0;
+    ListNode* h = head;
+    while (h) {
+      ++cnt;
+      h = h->next;
+    }
+    int midNode = (cnt / 2) + 1;
+    h = head;
+    for (int i = 1; h; ++i) {
+      if (i == midNode)
+        return h;
+      h = h->next;
+    }
+    return nullptr;
+  }
+};
+
 // Time  : O(n/2)
 // Space : O(1)
 
