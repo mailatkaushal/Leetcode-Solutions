9d313070eac3c4cfefc9da1b99d97d88518cd980
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-21 02:04:52 +0530

    find-length-of-loop

--- Linked-List/find-length-of-loop.cpp
+++ Linked-List/find-length-of-loop.cpp
@@ -13,3 +13,25 @@ int lengthOfLoop(Node *head) {
   }
   return 0;
 }
+
+// Time  : O(n)
+// Space : O(1)
+
+int lengthOfLoop(Node *head) {
+  Node* fast = head;
+  Node* slow = head;
+  while (fast && fast->next) {
+    fast = fast->next->next;
+    slow = slow->next;
+    if (fast == slow) {
+      int len = 1;
+      slow = slow->next;
+      while (slow != fast) {
+        ++len;
+        slow = slow->next;
+      }
+      return len;
+    }
+  }
+  return 0;
+}
