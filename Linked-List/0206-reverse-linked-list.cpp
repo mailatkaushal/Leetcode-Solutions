ac8a788dc1c68432c5db2a6d6e2d411a00aafc35
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-20 16:32:30 +0530

    reverse-linked-list

---
+++ Linked-List/0206-reverse-linked-list.cpp
@@ -0,0 +1,16 @@
+// Time  : O(n)
+// Space : O(1)
+
+class Solution {
+public:
+  ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    while (head) {
+      ListNode* next = head->next;
+      head->next = prev;
+      prev = head;
+      head = next;
+    }
+    return prev;
+  }
+};
