6786aa786b79de4484741e5cd9c5c44c9b09badf
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-22 02:53:12 +0530

    update

--- Linked-List/remove-duplicates-from-a-sorted-doubly-linked-list.cpp
+++ Linked-List/remove-duplicates-from-a-sorted-doubly-linked-list.cpp
@@ -3,19 +3,15 @@
 
 Node* removeDuplicates(Node* head) {
   Node* h = head;
-  while (h->next) {
-    Node* i = h;
-    while (i && i->data == h->data) 
+  while (h && h->next) {
+    Node* i = h->next;
+    while (i && i->data == h->data) {
       i = i->next;
-    // if i reaches unequal node
-    if (i != nullptr) {
-      h->next = i;
-      i->prev = h;
-      h = h->next;
     }
-    // if i reaches nullptr
-    else 
-      h->next = nullptr;
+    h->next = i;
+    if (i)
+      i->prev = h;
+    h = h->next;
   }
   return head;
 }
