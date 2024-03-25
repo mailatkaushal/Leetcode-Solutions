da1b0512e05d1c2c9a28fbb9d7eb7d2671c15984
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-25 18:31:19 +0530

    rename

--- Maths/check-prime.cpp
+++ Maths/check-prime.cpp
@@ -24,3 +24,16 @@ bool isPrime(int n) {
 	}
 	return divisors == 2;
 }
+
+// Time  : O(sqrt(n))
+// Space : O(1)
+
+bool isPrime(int n) {
+	if (n == 1)
+		return false;
+	for (int i = 2; i*i < n; ++i) {
+		if (n % i == 0)
+			return false;
+	}
+	return true;
+}
