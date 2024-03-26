4be6c543cdd760daa4272eabdbaffda458cdf368
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-26 21:56:27 +0530

    update

--- Trees/0104-maximum-depth-of-binary-tree.cpp
+++ Trees/0104-maximum-depth-of-binary-tree.cpp
@@ -1,4 +1,26 @@
 // Time  : O(n)
+// Space : O(h)
+
+class Solution {
+public:
+  int max_lvl = 0;
+  void dfs(TreeNode* root, int lvl) {
+    if (root == NULL)
+      return;
+    if (root->left == NULL && root->right == NULL) 
+      max_lvl = max(max_lvl, lvl);
+    dfs(root->left, lvl+1);
+    dfs(root->right, lvl+1);
+  }
+  int maxDepth(TreeNode* root) {
+    if (root == nullptr)
+      return 0;
+    dfs(root, 1);
+    return max_lvl;
+  }
+};
+
+// Time  : O(n)
 // Space : O(n)
 
 class Solution {
