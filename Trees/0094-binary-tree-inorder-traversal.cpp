e8b268935ff7b1d1c3d8de9a171a0c6724557755
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-26 19:43:45 +0530

    update

--- Trees/0094-binary-tree-inorder-traversal.cpp
+++ Trees/0094-binary-tree-inorder-traversal.cpp
@@ -16,3 +16,25 @@ public:
     return in;
   }
 };
+
+// Time  : O(n)
+// Space : O(n)
+
+class Solution { 
+public:
+  vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> in;
+    stack<TreeNode*> stk;
+    while (root || !stk.empty()) {
+      while (root) {
+        stk.push(root);
+        root = root->left;
+      }
+      root = stk.top();
+      stk.pop();
+      in.push_back(root->val);
+      root = root->right;
+    }
+    return in;
+  }
+};
