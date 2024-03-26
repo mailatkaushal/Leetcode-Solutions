5162d92f418aa34f90021ea8f5191b548adfaa1c
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-26 17:05:45 +0530

    update

--- Trees/0144-binary-tree-preorder-traversal.cpp
+++ Trees/0144-binary-tree-preorder-traversal.cpp
@@ -16,3 +16,27 @@ public:
     return pre;
   }
 };
+
+// Time  : O(n)
+// Space : O(n)
+
+class Solution {
+public:
+  vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> pre;
+    if (root == NULL)
+      return pre;
+    stack<TreeNode*> stk;
+    stk.push(root);
+    while (!stk.empty()) {
+      TreeNode* root = stk.top();
+      stk.pop();
+      pre.push_back(root->val);
+      if (root->right)
+        stk.push(root->right);
+      if (root->left)
+        stk.push(root->left);
+    }
+    return pre;
+  }
+};
