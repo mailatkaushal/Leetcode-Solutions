0752693af8bd0f274ad02d525a8c3f8a81f91719
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-28 00:19:18 +0530

    balanced-binary-tree

---
+++ Trees/0110-balanced-binary-tree.cpp
@@ -0,0 +1,24 @@
+// Time  : O(n)
+// Space : O(n)
+
+class Solution {
+public:
+  int height(TreeNode* root) {
+    if (root == NULL)
+      return 0;
+    return 1 + max(height(root->left), height(root->right));
+  }
+  bool isBalanced(TreeNode* root) {
+    if (root == NULL)
+      return true;
+    bool lBal = isBalanced(root->left);
+    if (lBal == false)
+      return false;
+    bool rBal = isBalanced(root->right);
+    if (rBal == false)
+      return false;
+    int lh = height(root->left);
+    int rh = height(root->right);
+    return abs(lh - rh) <= 1;
+  }
+};
