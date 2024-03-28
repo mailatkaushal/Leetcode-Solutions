2f6167c21290e492a76c5a3f02e4484a1a0b8af1
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-28 21:34:39 +0530

    binary-tree-right-side-view

---
+++ Trees/0199-binary-tree-right-side-view.cpp
@@ -0,0 +1,27 @@
+// Time  : O(n)
+// Space : O(n)
+
+class Solution {
+public:
+  vector<int> rightSideView(TreeNode* root) {
+    vector<int> r;
+    if (root == NULL)
+      return r;
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()) {
+      int n = q.size();
+      for (int i = 0; i < n; ++i) {
+        TreeNode* node = q.front();
+        q.pop();
+        if (i == n-1) 
+          r.push_back(node->val);
+        if (node->left)
+          q.push(node->left);
+        if (node->right)
+          q.push(node->right);
+      }
+    }
+    return r;
+  }
+};
