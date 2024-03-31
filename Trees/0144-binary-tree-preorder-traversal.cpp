92f8cd17c9a82690dc84a5a5ad3b000561821c21
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-03-31 15:25:26 +0530

    added morris traversal

--- Trees/0144-binary-tree-preorder-traversal.cpp
+++ Trees/0144-binary-tree-preorder-traversal.cpp
@@ -40,3 +40,36 @@ public:
     return pre;
   }
 };
+
+// Morris Traversal
+// Time  : O(n)
+// Space : O(1)
+
+class Solution {
+public:
+  vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> pre;
+    TreeNode* curr = root;
+    while (curr) {
+      if (curr->left == NULL) {
+        pre.push_back(curr->val);
+        curr = curr->right;
+      } 
+      else {
+        TreeNode* prev = curr->left;
+        while (prev->right && prev->right != curr) 
+          prev = prev->right;
+        if (prev->right == NULL) {
+          prev->right = curr;
+          pre.push_back(curr->val);
+          curr = curr->left;
+        }
+        else {
+          prev->right = NULL;
+          curr = curr->right;
+        }
+      }
+    }
+    return pre;
+  }
+};
