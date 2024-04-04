607dc4ca178f6acf8c6a7d6020d843372486a2e4
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-04 20:23:14 +0530

    fruit-into-baskets

---
+++ Sliding-Window/0904-fruit-into-baskets.cpp
@@ -0,0 +1,43 @@
+// Time  : O(2n)
+// Space : O(3) ~ O(1)
+
+class Solution {
+public:
+  int totalFruit(vector<int>& A) {
+    int n = A.size();
+    int ma = 0;
+    unordered_map<int, int> ump;
+    for (int i = 0, j = 0; j < n; ++j) {
+      ++ump[A[j]];
+      while (ump.size() > 2) {
+        --ump[A[i]];
+        if (ump[A[i]] == 0) ump.erase(A[i]);
+        ++i;
+      }
+      ma = max(ma, j-i+1);
+    }
+    return ma;
+  }
+};
+
+// Time  : O(2n)
+// Space : O(3) ~ O(1)
+
+class Solution {
+public:
+  int totalFruit(vector<int>& A) {
+    int n = A.size();
+    int ma = 0;
+    unordered_map<int, int> ump;
+    for (int i = 0, j = 0; j < n; ++j) {
+      ++ump[A[j]];
+      if (ump.size() > 2) {
+        --ump[A[i]];
+        if (ump[A[i]] == 0) ump.erase(A[i]);
+        ++i;
+      }
+      ma = max(ma, j-i+1);
+    }
+    return ma;
+  }
+};
