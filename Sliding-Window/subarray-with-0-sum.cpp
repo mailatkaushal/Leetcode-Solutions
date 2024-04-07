c3020a316a7390de3cb623cb8706b8437d9e152e
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-07 23:27:07 +0530

    in

--- Sliding-Window/subarray-with-0-sum.cpp
+++ Sliding-Window/subarray-with-0-sum.cpp
@@ -4,14 +4,14 @@
 class Solution{
 public:
   bool subArrayExists(int A[], int n) {
-      unordered_set<int> st;
-      st.insert(0);
-      int sum = 0;
-      for (int i = 0; i < n; ++i) {
-        sum += A[i];
-        if (st.find(sum) != st.end()) return true;
-        st.insert(sum);
-      }
-      return false;
+    unordered_set<int> st;
+    st.insert(0);
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+      sum += A[i];
+      if (st.find(sum) != st.end()) return true;
+      st.insert(sum);
+    }
+    return false;
   }
 };
