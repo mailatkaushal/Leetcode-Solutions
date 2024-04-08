3c811477129a6a07983c61dab2b1959b59626954
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-08 23:39:00 +0530

    sum-root-to-leaf-numbers

---
+++ Trees/0129-sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,37 @@
+// Time  : O(n)
+// Space : O(n)
+
+class Solution {
+public:
+  int sumNumbers(TreeNode* root) {
+    int sum = 0;
+    unordered_map<TreeNode*, TreeNode*> parent;
+    parent[root] = NULL;
+    stack<TreeNode*> st;
+    st.push(root);
+    while (!st.empty()) {
+      TreeNode* node = st.top();
+      st.pop();
+      if (node->left == NULL && node->right == NULL) {
+        int i = 0;
+        TreeNode* cur = node;
+        int num = 0;
+        while (cur) {
+          num += cur->val * pow(10, i);
+          cur = parent[cur];
+          ++i;
+        }
+        sum += num;
+      }
+      if (node->right) {
+        parent[node->right] = node;
+        st.push(node->right);
+      }
+      if (node->left) {
+        parent[node->left] = node;
+        st.push(node->left);
+      }
+    }
+    return sum;
+  }
+};
