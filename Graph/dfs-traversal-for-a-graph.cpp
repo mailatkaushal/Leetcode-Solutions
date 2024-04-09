4a26716c1f1a129b402477526e43d0ea942f2c69
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-10 01:53:49 +0530

    dfs-traversal-for-a-graph

---
+++ Graph/dfs-traversal-for-a-graph.cpp
@@ -0,0 +1,22 @@
+// Time  : O(n+2e)
+// Space : O(n)
+
+class Solution {
+public:
+  // Function to return a list containing the DFS traversal of the graph.
+  void dfs(int u, vector<int> adj[], vector<bool>& visited, vector<int>& res) {
+    visited[u] = 1;
+    res.push_back(u);
+    for (int& v : adj[u]) {
+      if (!visited[v]) 
+        dfs(v, adj, visited, res);
+    }
+  }
+
+  vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+    vector<int> res;
+    vector<bool> visited(V);
+    dfs(0, adj, visited, res);
+    return res;
+  }
+};
