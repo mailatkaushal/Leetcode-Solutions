f86127f0e1735e417357421606187f76cc417407
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-10 21:44:57 +0530

    detect-cycle-in-an-undirected-graph

---
+++ Graph/detect-cycle-in-an-undirected-graph.cpp
@@ -0,0 +1,31 @@
+// Time  : O(n^2)
+// Time  : O(n)
+
+class Solution {
+public:
+  // Function to detect cycle in an undirected graph.
+  bool isCycle(int V, vector<int> adj[]) {
+    vector<bool> vis(V);
+    for (int i = 0; i < V; ++i) {
+      if (!vis[i]) {
+        vis[i] = 1;
+        queue<pair<int, int>> q;
+        q.push({i, -1});
+        while (!q.empty()) {
+          auto p = q.front();
+          q.pop();
+          int u = p.first;
+          int parent = p.second;
+          for (int& v : adj[u]) {
+            if (!vis[v]) {
+              q.push({v, u});
+              vis[v] = 1;
+            }
+            else if (parent != v) return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+};
