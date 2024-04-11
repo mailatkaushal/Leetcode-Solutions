77f5f1e293a3474b723a673f8240eedcccffaffa
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-11 13:27:37 +0530

    update

--- Graph/0547-number-of-provinces.cpp
+++ Graph/0547-number-of-provinces.cpp
@@ -1,25 +1,24 @@
 // Time  : O(n^2)
 // Space : O(n)
-
 class Solution {
 public:
   int findCircleNum(vector<vector<int>>& isConnected) {
     int n = isConnected.size();
     int cnt = 0;
+    vector<bool> vis(n);
     queue<int> q;
-    vector<bool> visited(n);
-    for (int u = 0; u < n; ++u) {
-      if (!visited[u]) {
+    for (int i = 0; i < n; ++i) {
+      if (!vis[i]) {
         ++cnt;
-        q.push(u);
-        visited[u] = 1;
+        vis[i] = 1;
+        q.push(i);
         while (!q.empty()) {
           int u = q.front();
           q.pop();
-          for (int v = 0; v < n; ++v) {
-            if (isConnected[u][v] && !visited[v]) {
-              visited[v] = 1;
-              q.push(v);
+          for (int i = 0; i < n; ++i) {
+            if (isConnected[u][i] && !vis[i]) {
+              vis[i] = 1;
+              q.push(i);
             }
           }
         }
@@ -31,34 +30,33 @@ public:
 
 // Time  : O(n^2)
 // Space : O(n)
-
 class Solution {
 public:
   int findCircleNum(vector<vector<int>>& isConnected) {
     int n = isConnected.size();
     int cnt = 0;
     vector<vector<int>> adj(n);
-    for (int u = 0; u < n; ++u) {
-      for (int v = 0; v < n; ++v) {
-        if (isConnected[u][v]) {
-          adj[u].push_back(v);
-          adj[v].push_back(u);
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (isConnected[i][j]) {
+          adj[i].push_back(j);
+          adj[j].push_back(i);
         }
       }
     }
+    vector<bool> vis(n);
     queue<int> q;
-    vector<bool> visited(n);
-    for (int u = 0; u < n; ++u) {
-      if (!visited[u]) {
+    for (int i = 0; i < n; ++i) {
+      if (!vis[i]) {
         ++cnt;
-        visited[u] = 1;
-        q.push(u);
+        vis[i] = 1;
+        q.push(i);
         while (!q.empty()) {
           int u = q.front();
           q.pop();
           for (int& v : adj[u]) {
-            if (!visited[v]) {
-              visited[v] = 1;
+            if (!vis[v]) {
+              vis[v] = 1;
               q.push(v);
             }
           }
@@ -70,35 +68,32 @@ public:
 };
 
 // Time  : O(n^2)
-// Space : O(n)
-
-class Solution {
+// Space : O(n)class Solution {
 public:
-  void dfs(int u, vector<vector<int>>& adj, vector<bool>& visited) {
-    visited[u] = 1;
+  int cnt = 0;
+  void dfs(int u, vector<vector<int>>& adj, vector<bool>& vis) {
+    vis[u] = 1;
     for (int& v : adj[u]) {
-      if (!visited[v])
-        dfs(v, adj, visited);
+      if (!vis[v]) 
+        dfs(v, adj, vis);
     }
   }
-
   int findCircleNum(vector<vector<int>>& isConnected) {
     int n = isConnected.size();
-    int cnt = 0;
     vector<vector<int>> adj(n);
-    for (int u = 0; u < n; ++u) {
-      for (int v = 0; v < n; ++v) {
-        if (isConnected[u][v]) {
-          adj[u].push_back(v);
-          adj[v].push_back(u);
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        if (isConnected[i][j]) {
+          adj[i].push_back(j);
+          adj[j].push_back(i);
         }
       }
     }
-    vector<bool> visited(n);
-    for (int u = 0; u < n; ++u) {
-      if (!visited[u]) {
+    vector<bool> vis(n);
+    for (int i = 0; i < n; ++i) {
+      if (!vis[i]) {
         ++cnt;
-        dfs(u, adj, visited);
+        dfs(i, adj, vis);
       }
     }
     return cnt;
