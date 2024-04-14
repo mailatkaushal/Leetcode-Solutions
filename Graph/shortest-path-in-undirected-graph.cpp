0ea0685e65ee18c377366965a9644c7089ffec89
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-14 19:32:34 +0530

    update

--- Graph/shortest-path-in-undirected-graph.cpp
+++ Graph/shortest-path-in-undirected-graph.cpp
@@ -2,41 +2,41 @@
 // Space : O(n^2)
 class Solution {
 public:
-  void topoSort(int u, vector<vector<pair<int, int>>>& adj, vector<bool>& vis, stack<int>& st) {
-    vis[u] = 1;
-    for (auto& p : adj[u]) {
-      int v = p.first;
-      if (!vis[v]) 
-        topoSort(v, adj, vis, st);
-    }
-    st.push(u);
-  }
-  vector<int> shortestPath(int n,int m, vector<vector<int>>& edges){
+  vector<int> shortestPath(int n,int m, vector<vector<int>>& edges) {
     vector<vector<pair<int, int>>> adj(n);
     vector<int> indeg(n);
-    for (auto& e : edges) adj[e[0]].push_back({e[1], e[2]});
-    vector<bool> vis(n);
-    stack<int> st;
-    for (int i = 0; i < n; ++i) {
-      if (!vis[i]) {
-        topoSort(i, adj, vis, st);
+    for (auto& e : edges) {
+      int u = e[0];
+      int v = e[1];
+      int w = e[2];
+      adj[u].push_back({v, w});
+      ++indeg[v];
+    }
+    queue<int> q;
+    for (int i = 0; i < n; ++i) if (indeg[i] == 0) q.push(i);
+    vector<int> vec(n);
+    int x = 0;
+    while (!q.empty()) {
+      int u = q.front();
+      q.pop();
+      vec[x++] = u;
+      for (auto& p : adj[u]) {
+        int v = p.first;
+        if (--indeg[v] == 0) q.push(v);
       }
     }
+    x = 0;
     vector<int> dist(n, 1e9);
     dist[0] = 0;
-    while (!st.empty()) {
-      int u = st.top();
-      st.pop();
+    while (x < n) {
+      int u = vec[x++];
       for (auto& p : adj[u]) {
         int v = p.first;
         int w = p.second;
         dist[v] = min(dist[v], dist[u] + w);
       }
     }
-    for (int i = 0; i < n; ++i) {
-      if (dist[i] == 1e9)
-        dist[i] = -1;
-    }
+    for (int i = 0; i < n; ++i) if (dist[i] == 1e9) dist[i] = -1;
     return dist;
   }
 };
