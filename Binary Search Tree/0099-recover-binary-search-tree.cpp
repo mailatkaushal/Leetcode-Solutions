3cd0780e795775ec6f38cf7d220f41095bba8ab3
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-16 02:44:36 +0530

    optimal sol

--- Binary Search Tree/0099-recover-binary-search-tree.cpp
+++ Binary Search Tree/0099-recover-binary-search-tree.cpp
@@ -1,13 +1,19 @@
-// Time  : O(2n + nlogn)
-// Space : O(logn)
+// Time  : O(n)
+// Space : O(1)
 class Solution {
 public:
   void recoverTree(TreeNode* root) {
-    vector<int> A;
+    TreeNode* p = NULL;
+    TreeNode* x = NULL;
+    TreeNode* y = NULL;
     TreeNode* cur = root;
     while (cur) {
       if (cur->left == NULL) {
-        A.push_back(cur->val);
+        if (p && p->val > cur->val) {
+          if (x == NULL) x = p;
+          y = cur;
+        }
+        p = cur;
         cur = cur->right;
       }
       else {
@@ -20,33 +26,15 @@ public:
         }
         else {
           pre->right = NULL;
-          A.push_back(cur->val);
+          if (p && p->val > cur->val) {
+            if (!x) x = p;
+            y = cur; 
+          }
+          p = cur;
           cur = cur->right;
         }
       }
     }
-    sort(A.begin(), A.end());
-    cur = root;
-    int i = 0;
-    while (cur) {
-      if (cur->left == NULL) {
-        cur->val = A[i++];
-        cur = cur->right;
-      }
-      else {
-        TreeNode* pre = cur->left;
-        while (pre->right && pre->right != cur) 
-          pre = pre->right;
-        if (pre->right == NULL) {
-          pre->right = cur;
-          cur = cur->left;
-        }
-        else {
-          pre->right = NULL;
-          cur->val = A[i++];
-          cur = cur->right;
-        }
-      }
-    }
+    swap(x->val, y->val);
   }
 };
