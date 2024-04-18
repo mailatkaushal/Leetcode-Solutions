198873af9e0d24e9c9e6bf18aba57df698f97645
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-18 14:32:54 +0530

    added iterative sol

--- Graph/0463-island-perimeter.py
+++ Graph/0463-island-perimeter.py
@@ -1,29 +1,4 @@
 # Time  : O(m*n)
-# Space : O(1)
-class Solution:
-  def islandPerimeter(self, grid: List[List[int]]) -> int:
-    m, n = len(grid), len(grid[0])
-
-    def dfs(i, j):
-      if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == 0:
-        return 1
-      if grid[i][j] == -1:
-        return 0
-
-      grid[i][j] = -1
-
-      perimeter = dfs(i + 1, j)
-      perimeter += dfs(i - 1, j)
-      perimeter += dfs(i, j + 1)
-      perimeter += dfs(i, j - 1)
-      return perimeter
-
-    for i in range(m):
-      for j in range(n):
-        if grid[i][j] == 1:
-          return dfs(i, j)
-
-# Time  : O(m*n)
 # Space : O(m*n)
 class Solution {
 public:
@@ -59,3 +34,26 @@ public:
     return 0;
   }
 };
+
+# Iterative
+# Time  : O(m*n)
+# Space : O(1)
+class Solution:
+  def islandPerimeter(self, grid: List[List[int]]) -> int:
+    m, n = len(grid), len(grid[0])
+    perimeter = 0
+    
+    for i in range(m):
+      for j in range(n):
+        if grid[i][j] == 0:
+          continue
+        if i+1 >= m or grid[i+1][j] == 0:
+          perimeter += 1
+        if i-1 < 0 or grid[i-1][j] == 0:
+          perimeter += 1
+        if j+1 >= n or grid[i][j+1] == 0:
+          perimeter += 1
+        if j-1 < 0 or grid[i][j-1] == 0:
+          perimeter += 1
+
+    return perimeter
