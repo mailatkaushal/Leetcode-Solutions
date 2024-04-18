4009d27a62084f3e8f3f2fb8e646226c3e44ce6c
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-18 22:20:32 +0530

    update

--- Graph/shortest-path-in-undirected-graph-having-unit-distance.cpp
+++ Graph/shortest-path-in-undirected-graph-having-unit-distance.cpp
@@ -2,26 +2,28 @@
 // Space : O(n)
 class Solution {
 public:
-  vector<int> shortestPath(vector<vector<int>>& edges, int n ,int m, int src) {
+  vector<int> shortestPath(vector<vector<int>>& edges, int n, int m, int src) {
     vector<vector<int>> adj(n);
     for (auto& e : edges) {
-        adj[e[0]].push_back(e[1]);
-        adj[e[1]].push_back(e[0]);
+      adj[e[0]].push_back(e[1]);
+      adj[e[1]].push_back(e[0]);
     }
     vector<int> dist(n, -1);
     dist[src] = 0;
-    vector<bool> vis(n);
-    vis[src] = 1;
     queue<int> q;
     q.push(src);
+    int i = 0;
     while (!q.empty()) {
-      int u = q.front();
-      q.pop();
-      for (int& v : adj[u]) {
-        if (!vis[v]) {
-          vis[v] = 1;
-          q.push(v);
-          dist[v] = dist[u] + 1;
+      ++i;
+      int k = q.size();
+      while (k--) {
+        int u = q.front();
+        q.pop();
+        for (int& v : adj[u]) {
+          if (dist[v] == -1) {
+            dist[v] = i;
+            q.push(v);
+          }
         }
       }
     }
