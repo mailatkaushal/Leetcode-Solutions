47917516460b5b3b45534f25b86aad380a9d00cf
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-18 23:57:03 +0530

    update

--- Graph/0787-cheapest-flights-within-k-stops.cpp
+++ Graph/0787-cheapest-flights-within-k-stops.cpp
@@ -5,27 +5,27 @@ public:
   int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
     vector<vector<pair<int, int>>> adj(n);
     for (auto& f : flights) adj[f[0]].push_back({f[1], f[2]});
+    vector<int> D(n, 1e9);
+    D[src] = 0;
     queue<pair<int, int>> q;
-    vector<int> dist(n, 1e9);
     q.push({src, 0});
-    dist[src] = 0;
-    int stops = -1;
+    int stops = 0;
     while (!q.empty()) {
-      ++stops;
       if (stops > k) break;
-      int x = q.size();
-      while (x--) {
+      int z = q.size();
+      while (z--) {
         auto [u, du] = q.front();
         q.pop();
         for (auto& [v, duv] : adj[u]) {
-          if (du + duv < dist[v]) {
-            dist[v] = du + duv;
-            q.push({v, dist[v]});
+          if (du + duv < D[v]) {
+            D[v] = du + duv;
+            q.push({v, D[v]});
           }
         }
       }
+      ++stops;
     }
-    if (dist[dst] == 1e9) return -1;
-    return dist[dst];
+    if (D[dst] == 1e9) return -1;
+    return D[dst];
   }
 };
