ebe74328dd2c7a6cada471ed9fcd2afa14c21c56
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-04-27 18:22:32 +0530

    update

--- Trees/diagonal-traversal-of-binary-tree.cpp
+++ Trees/diagonal-traversal-of-binary-tree.cpp
@@ -1,20 +1,16 @@
 // Time  : O(n)
 // Space : O(n)
-
 vector<int> diagonal(Node *root) {
   vector<int> res;
   queue<Node*> q;
   q.push(root);
   while (!q.empty()) {
-    int n = q.size();
-    while (n--) {
-      Node* node = q.front();
-      q.pop();
-      while (node) {
-        res.push_back(node->data);
-        if (node->left) q.push(node->left);
-        node = node->right;
-      }
+    Node* node = q.front();
+    q.pop();
+    while (node) {
+      res.push_back(node->data);
+      if (node->left) q.push(node->left);
+      node = node->right;
     }
   }
   return res;
