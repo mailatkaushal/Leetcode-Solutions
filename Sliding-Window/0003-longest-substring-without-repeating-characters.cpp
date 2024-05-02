31abdb2b3b239416b7ab0a3de17666a97672a543
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-05-02 20:37:54 +0530

    update

--- Sliding-Window/0003-longest-substring-without-repeating-characters.cpp
+++ Sliding-Window/0003-longest-substring-without-repeating-characters.cpp
@@ -1,41 +1,17 @@
-// Time  : O(2n)
-// Space : O(n)
-
-class Solution {
-public:
-  int lengthOfLongestSubstring(string& s) {
-    int n = s.size();
-    int ma = 0;
-    unordered_set<char> st;
-    for (int l = 0, r = 0; r < n; ++r) {
-      while (st.find(s[r]) != st.end()) {
-        st.erase(s[l]);
-        ++l;
-      }
-      st.insert(s[r]);
-      ma = max(ma, r-l+1);
-    }
-    return ma;
-  }
-};
-
 // Time  : O(n)
 // Space : O(n)
-
 class Solution {
 public:
   int lengthOfLongestSubstring(string& s) {
-    int n = s.size();
-    int ma = 0;
-    vector<int> v(256,-1);
-    for (int l = 0, r = 0; r < n; ++r) {
-      if (v[s[r]] != -1) {
-        if (v[s[r]] + 1 > l)
-          l = v[s[r]] + 1; 
-      } 
-      v[s[r]] = r;
-      ma = max(ma, r - l + 1);
+    int ans = 0;
+    vector<int> v(256, -1);
+    for (int i = 0, j = 0; j < s.size(); ++j) {
+      if (v[s[j]] != -1) {
+        i = max(i, v[s[j]] + 1);
+      }
+      v[s[j]] = j;
+      ans = max(ans, j-i+1);
     }
-    return ma;
+    return ans;
   }
 };
