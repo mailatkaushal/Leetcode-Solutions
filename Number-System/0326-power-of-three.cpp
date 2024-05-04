ada7fa1d2cb22a98a9bac3f7c0d264a3a7bfd0c2
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-05-04 22:23:16 +0530

    update

--- Number-System/0326-power-of-three.cpp
+++ Number-System/0326-power-of-three.cpp
@@ -1,19 +1,8 @@
-// Time  : O(logn)
-// Space : O(1)
-
-class Solution:
-  def isPowerOfTwo(self, n: int) -> bool:
-    x = 1
-    while x < n:
-      x *= 3
-    return x == n
-
 // Time  : O(1)
 // Space : O(1)
-
-class Solution:
-  def isPowerOfThree(self, n: int) -> bool:
-    if n <= 0:
-      return False
-
-    return 1162261467 % n == 0  #  3^19 = 1162261467
+class Solution {
+public:
+  bool isPowerOfThree(int n) {
+    return n > 0 && 1162261467 % n == 0;  // 3^19 = 1162261467
+  }
+};
