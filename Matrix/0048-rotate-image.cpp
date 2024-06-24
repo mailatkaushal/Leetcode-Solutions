4a6cf0e7dc9e0ea42f6ba90d4452c35197377dae
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-06-24 12:22:30 +0530

    update

--- Matrix/0048-rotate-image.cpp
+++ Matrix/0048-rotate-image.cpp
@@ -1,25 +1,18 @@
 // Time  : O(n^2)
 // Space : O(1)
-
-class Solution:
-  def rotate(self, matrix: List[List[int]]) -> None:
-    '''
-      1  2  3      7  8  9      7  4  1
-      4  5  6  ->  4  5  6  ->  8  5  2
-      7  8  9      1  2  3      9  6  3
-    '''
-
-    n = len(matrix)
-
-    # reverse each row
-    start, end = 0, n - 1
-    while start < end:
-      for i in range(n):
-        matrix[start][i], matrix[end][i] = matrix[end][i], matrix[start][i]
-      start += 1
-      end -= 1
-
-    # Transpose the matrix (swap non-diagonal elements)
-    for i in range(n):
-      for j in range(i):
-        matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+class Solution {
+public:
+  void rotate(vector<vector<int>>& M) {
+    int m = M.size();
+    int n = M[0].size();
+    for (int i=0; i<m; ++i) {
+      for (int j=0; j<i; ++j) {
+        swap(M[i][j], M[j][i]);
+      }
+    }
+    for (int i=0; i<m; ++i) {
+      int x = 0, y = n-1;
+      while (x < y) swap(M[i][x++], M[i][y--]);
+    }
+  }
+};
