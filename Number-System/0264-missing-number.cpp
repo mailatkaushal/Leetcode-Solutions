b65f0bd25eb1e75f39d1ddcf26fa1bd90073e00b
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-06-26 17:00:44 +0530

    update

--- Number-System/0264-missing-number.cpp
+++ Number-System/0264-missing-number.cpp
@@ -4,8 +4,7 @@ class Solution {
 public:
   int missingNumber(vector<int>& A) {
     int ret = 0;
-    for (int i=0; i<A.size(); ++i) ret ^= i ^ A[i];
-    ret ^= A.size();
+    for (int i=0; i<A.size(); ++i) ret ^= (i+1) ^ A[i];
     return ret;
   }
 };
