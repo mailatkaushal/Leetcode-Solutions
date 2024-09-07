5366d2acb5665970474d8bf7cf2ec0abf598d932
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-09-07 23:53:32 +0530

    update

--- Arrays/kadanes-algorithm.cpp
+++ Arrays/kadanes-algorithm.cpp
@@ -1,14 +1,10 @@
-class Solution {
-public:
-  int maxSubArray(vector<int>& A) {
-    int n = A.size();
-    int ans = INT_MIN;
-    int x = 0;
-    for (int i=0; i<n; ++i) {
-      x += A[i];
-      ans = max(ans, x);
-      if (x < 0) x = 0;
+long long maxSubarraySum(vector<int>& A) {
+    long long ans=LLONG_MIN;
+    long long sum=0;
+    for(int i=0;i<A.size();++i){
+        sum+=A[i];
+        ans=max(ans,sum);
+        if(sum<0) sum=0;
     }
     return ans;
-  }
-};
+}
