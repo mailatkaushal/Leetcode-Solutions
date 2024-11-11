8e41ab76309e91efe5043ff22669353d6f592f85
Kaushal Kumar <134132345+mailatkaushal@users.noreply.github.com>
2024-11-12 00:39:29 +0530

    update

--- Maths/sieve-of-eratosthenes.cpp
+++ Maths/sieve-of-eratosthenes.cpp
@@ -4,12 +4,12 @@ class Solution {
 public:
     int countPrimes(int n) {
         int ans=0;
-        vector<bool> v(n+1,1);
+        vector<bool> seive(n+1,1);
         for (int i=2; i<n; ++i) {
-            if (v[i]==1) {
+            if (seive[i]==1) {
                 ans++;
                 for (int j=2*i; j<n; j+=i) {
-                    v[j]=0;
+                    seive[j]=0;
                 }
             }
         }
